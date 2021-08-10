948fdc931e41e65a6f069bbb1b0506ce4e0efaf5
offbyone <offbyone@disroot.org>
2021-08-10 21:02:57 +0200

    Initial commit

---
+++ src/main/java/com/github/klainstom/ExtensionMain.java
@@ -0,0 +1,15 @@
+package com.github.klainstom;
+
+import net.minestom.server.extensions.Extension;
+
+public class ExtensionMain extends Extension {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void terminate() {
+
+    }
+}
