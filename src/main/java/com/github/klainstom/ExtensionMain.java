378243b3083d013c0fc09d6dae3a74c895a854d3
offbyone <offbyone@disroot.org>
2021-08-12 17:22:02 +0200

    Move the ExtensionMain.java to package named after extension

--- src/main/java/com/github/klainstom/ExtensionMain.java
+++ src/main/java/com/github/klainstom/ExtensionMain.java
@@ -3,6 +3,7 @@ package com.github.klainstom;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.extensions.Extension;
 
+// TODO: 12.08.21 move to actual extension package
 public class ExtensionMain extends Extension {
     @Override
     public void initialize() {
