e3195eff602259fd906d3e8d3ac260905232988f
offbyone <offbyone@disroot.org>
2022-02-18 17:14:01 +0100

    Change basic structure of the project

--- src/main/java/com/github/klainstom/ExtensionMain.java
+++ src/main/java/com/github/klainstom/ExtensionMain.java
@@ -3,17 +3,41 @@ package com.github.klainstom;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.extensions.Extension;
 
-// TODO: 12.08.21 move to actual extension package
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
 public class ExtensionMain extends Extension {
+    public static Path DATA_DIRECTORY;
+
+    @Override
+    public void preInitialize() {
+         DATA_DIRECTORY = Objects.requireNonNull(MinecraftServer.getExtensionManager()
+                 .getExtension("$Name"), "Extension installed but not found!").getDataDirectory();
+
+        if (!DATA_DIRECTORY.toFile().exists()) {
+            try {
+                Files.createDirectory(DATA_DIRECTORY);
+            } catch (IOException e) {
+                MinecraftServer.LOGGER.error("Could not create terminus data directory", e);
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     public void initialize() {
-        MinecraftServer.LOGGER.info("$name$ initialize.");
+        Settings.read();
 
+        MinecraftServer.LOGGER.info("====== &NAME ======");
+        Info.printVersionLines();
+        Info.printSettingsLines();
+        MinecraftServer.LOGGER.info("======================");
     }
 
     @Override
     public void terminate() {
-        MinecraftServer.LOGGER.info("$name$ terminate.");
 
     }
 }
--- src/main/java/com/github/klainstom/data/ExtensionData.java
+++
@@ -1,5 +0,0 @@
-package com.github.klainstom.data;
-
-public class ExtensionData {
-    // TODO: 18.08.21 rename to <extension-name>Data
-}
