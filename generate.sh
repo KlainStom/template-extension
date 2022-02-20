174976490b0e25cc3aa224ddcfef3f2f16e30a3c
offbyone <offbyone@disroot.org>
2022-02-20 12:18:31 +0100

    Shorten generate.sh

--- generate.sh
+++ generate.sh
@@ -5,11 +5,8 @@ read name
 echo "Extension remote repository name: "
 read repo
 
-sed -i "s|        \"repo\": \"EXTENSION_REPO_NAME\",|        \"repo\": \"$repo\",|i" src/main/resources/extension.json
-sed -i "s|rootProject.name = \"EXTENSION_REPO_NAME\"|rootProject.name = \"$repo\"|i" settings.gradle.kts
-sed -i "s|var displayName = \"EXTENSION_DISPLAY_NAME\"|var displayName = \"$name\"|i" build.gradle.kts
-sed -i "s|EXTENSION_DISPLAY_NAME|$name|g" README.md
-sed -i "s|EXTENSION_REPO_NAME|$repo|g" README.md
+sed -i "s|EXTENSION_DISPLAY_NAME|$name|g" README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
+sed -i "s|EXTENSION_REPO_NAME|$repo|g" README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
 
 lowercasename="$(echo "$name" | tr '[:upper:]' '[:lower:]')"
 sed -i "s|package com.github.klainstom;|package com.github.klainstom.$lowercasename;|i" src/main/java/com/github/klainstom/*
