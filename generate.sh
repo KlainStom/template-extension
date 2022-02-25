baed1c26138188ed7b350a9a2145a4a63fb9d906
off-by-0point5 <61155885+off-by-0point5@users.noreply.github.com>
2022-02-25 18:23:24 +0100

    Add LICENSE  to the files with replacements

--- generate.sh
+++ generate.sh
@@ -8,10 +8,10 @@ echo "Extension remote repository owner: "
 read owner
 lowercaseowner="$(echo "$owner" | tr '[:upper:]' '[:lower:]' | tr -d '-')"
 
-sed -i "s|EXTENSION_DISPLAY_NAME|$name|g" README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
-sed -i "s|EXTENSION_REPO_NAME|$repo|g" README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
-sed -i "s|EXTENSION_REPO_OWNER|$owner|g" README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
-sed -i "s|EXTENSION_REPO_LOWER_OWNER|$lowercaseowner|g" README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
+sed -i "s|EXTENSION_DISPLAY_NAME|$name|g" LICENSE README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
+sed -i "s|EXTENSION_REPO_NAME|$repo|g" LICENSE README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
+sed -i "s|EXTENSION_REPO_OWNER|$owner|g" LICENSE README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
+sed -i "s|EXTENSION_REPO_LOWER_OWNER|$lowercaseowner|g" LICENSE README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
 
 lowercasename="$(echo "$name" | tr '[:upper:]' '[:lower:]')"
 sed -i "s|package com.github;|package com.github.$lowercaseowner.$lowercasename;|i" src/main/java/com/github/*
