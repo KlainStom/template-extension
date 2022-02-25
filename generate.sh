373b6f65110c039da7c74b6e97f53395ff02b722
offbyone <offbyone@disroot.org>
2022-02-25 18:06:22 +0100

    Fix project generation issues

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@ echo "Extension remote repository name: "
 read repo
 echo "Extension remote repository owner: "
 read owner
-lowercaseowner="$(echo "$owner" | tr '[:upper:]' '[:lower:]' | tr '-' '')"
+lowercaseowner="$(echo "$owner" | tr '[:upper:]' '[:lower:]' | tr -d '-')"
 
 sed -i "s|EXTENSION_DISPLAY_NAME|$name|g" README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
 sed -i "s|EXTENSION_REPO_NAME|$repo|g" README.md build.gradle.kts settings.gradle.kts src/main/resources/extension.json
@@ -15,7 +15,7 @@ sed -i "s|EXTENSION_REPO_LOWER_OWNER|$lowercaseowner|g" README.md build.gradle.k
 
 lowercasename="$(echo "$name" | tr '[:upper:]' '[:lower:]')"
 sed -i "s|package com.github;|package com.github.$lowercaseowner.$lowercasename;|i" src/main/java/com/github/*
-mkdir src/main/java/com/github/"$lowercaseowner"/"$lowercasename"/
+mkdir -p src/main/java/com/github/"$lowercaseowner"/"$lowercasename"/
 mv src/main/java/com/github/* src/main/java/com/github/"$lowercaseowner"/"$lowercasename"/
 
 rm generate.sh
